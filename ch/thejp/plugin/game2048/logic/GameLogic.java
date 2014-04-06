f8a226e657159866b1e004c2bbe3dfc3e679f448
TheJP <jp_55555@hotmail.com>
2014-04-06 13:58:54 +0200

    Worked on a generic moving algorithm

--- ch/thejp/plugin/game2048/logic/GameLogic.java
+++ ch/thejp/plugin/game2048/logic/GameLogic.java
@@ -2,14 +2,34 @@ package ch.thejp.plugin.game2048.logic;
 
 public class GameLogic implements IGameLogic {
 
+	/**
+	 * Class which represents a moving strategy to keep the moving algorithm more generic
+	 * @author JP
+	 */
+	private static class Strategy {
+		public int initX, initY, smallStepX, smallStepY;
+		public Strategy(int initX, int initY, int smallStepX, int smallStepY) {
+			super();
+			this.initX = initX;
+			this.initY = initY;
+			this.smallStepX = smallStepX;
+			this.smallStepY = smallStepY;
+		}
+	}
+
 	private IGameState gameState;
-	
+	private Strategy[] strategies = {
+		new Strategy(0, 0, 0, 1), //UP
+		new Strategy(GameState.FIELD_SIZE - 1, 0, -1, 0), //RIGHT
+		new Strategy(0, GameState.FIELD_SIZE - 1, 0, -1), //DOWN
+		new Strategy(0, 0, 1, 0)
+	};
 	
 	/**
 	 * Constructor needs valid gamestate
 	 * @param gameState
 	 */
-	public GameLogic(IGameState gameState){
+	public GameLogic(IGameState gameState) {
 		this.gameState = gameState;
 	}
 
@@ -24,24 +44,24 @@ public class GameLogic implements IGameLogic {
 	}
 
 	/**
-	 * Gets an int version of the Direction
+	 * Gets an Strategy version of the Direction
 	 * @param dir
 	 * @return
 	 */
-	private int getMovingStrategy(Direction dir){
+	private Strategy getMovingStrategy(Direction dir) {
 		switch(dir){
-		case Up:    return 0;
-		case Right: return 1;
-		case Down:  return 2;
-		case Left:  return 3;
-		default:    return 0;
+		case Up:    return strategies[0];
+		case Right: return strategies[1];
+		case Down:  return strategies[2];
+		case Left:  return strategies[3];
+		default:    return strategies[0];
 		}
 	}
 
 	@Override
 	public void move(Direction dir) {
 		//The main moving and scoring algorithm is defined here
-		int strategy = getMovingStrategy(dir);
+		Strategy strategy = getMovingStrategy(dir);
 	}
 
 }
