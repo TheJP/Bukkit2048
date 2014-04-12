c4d568e543b3e89b4ee2d0a9c3567816196d9c83
TheJP <jp_55555@hotmail.com>
2014-04-12 23:52:50 +0200

    Started implementation of highscore system

---
+++ src/ch/thejp/plugin/game2048/HighscoreManager.java
@@ -0,0 +1,28 @@
+package ch.thejp.plugin.game2048;
+
+/**
+ * Class to manage the highscore of the players
+ * @author JP
+ *
+ */
+public class HighscoreManager {
+
+	/**
+	 * Returns the highscore of the given player
+	 * @param playerName
+	 * @return
+	 */
+	public long get(String playerName){
+		return 0;
+	}
+
+	/**
+	 * Sets the highscore of the given player.
+	 * Only if the given highscore is higher the the existing
+	 * @param playerName
+	 * @param highscore
+	 */
+	public void set(String playerName, long highscore){
+		
+	}
+}
