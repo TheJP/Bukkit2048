e58f74a209053af8471c4a60d15e6572a0c7ad95
TheJP <jp_55555@hotmail.com>
2014-04-13 13:21:38 +0200

    Worked on highscore manager

--- src/ch/thejp/plugin/game2048/HighscoreManager.java
+++ src/ch/thejp/plugin/game2048/HighscoreManager.java
@@ -1,5 +1,11 @@
 package ch.thejp.plugin.game2048;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 /**
  * Class to manage the highscore of the players
  * @author JP
@@ -7,13 +13,17 @@ package ch.thejp.plugin.game2048;
  */
 public class HighscoreManager {
 
+	Map<String, Long> highscores = new HashMap<String, Long>();
+
 	/**
-	 * Returns the highscore of the given player
+	 * Returns the highscore of the given player or -1 if no highscore was found
 	 * @param playerName
 	 * @return
 	 */
 	public long get(String playerName){
-		return 0;
+		//Don't use auto boxing, because Long could be null
+		Long highscore = highscores.get(playerName);
+		return  (highscore == null ? -1 : highscore.longValue());
 	}
 
 	/**
@@ -23,6 +33,25 @@ public class HighscoreManager {
 	 * @param highscore
 	 */
 	public void set(String playerName, long highscore){
-		
+		//Add new highscore: if the player has no highscore or if the new highscore is higher then the old
+		if(!highscores.containsKey(playerName) || highscores.get(playerName).longValue() < highscore){
+			highscores.put(playerName, highscore);
+		}
+	}
+
+	/**
+	 * Returns the sorted highscores
+	 * @return
+	 */
+	public Entry<String, Long>[] getSorted(){
+		@SuppressWarnings("unchecked")
+		Entry<String, Long>[] result = (Entry<String, Long>[]) highscores.entrySet().toArray();
+		Arrays.sort(result, new Comparator<Entry<String, Long>>() {
+			@Override
+			public int compare(Entry<String, Long> o1, Entry<String, Long> o2) {
+				return o2.getValue().compareTo(o1.getValue());
+			}
+		});
+		return result;
 	}
 }
